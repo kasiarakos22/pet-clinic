679f2d84aefdb3aca0ad39b2cc46870298e46a2e
dkasiaras <dimitris.kasiaras@camelotglobal.com>
2018-09-19 17:08:06 +0300

    Add Vets List and update controller

--- pet-clinic-web/src/main/java/com/kasiarakos/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/kasiarakos/petclinic/controllers/VetController.java
@@ -1,13 +1,24 @@
 package com.kasiarakos.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.kasiarakos.petclinic.services.VetServicce;
 
 @Controller
 public class VetController {
 
+    private final VetServicce vetServicce;
+
+    public VetController(VetServicce vetServicce) {
+        this.vetServicce = vetServicce;
+    }
+
     @RequestMapping({"/vets", "vets/index", "vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets", vetServicce.findAll());
         return "vets/index";
     }
 }
